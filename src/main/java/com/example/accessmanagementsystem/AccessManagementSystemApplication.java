2bb80ad7676511e6384df0865bbf5013ced32344
Igor <igopood33@gmail.com>
2025-01-22 18:00:10 +0100

    initialized project

---
+++ src/main/java/com/example/accessmanagementsystem/AccessManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessManagementSystemApplication.class, args);
+	}
+
+}
