ef42ff0d21f2936f2ad4ce2f1959f256fcce8b97
Igor <igopood33@gmail.com>
2025-01-28 19:16:39 +0100

    lombok constructor

--- src/main/java/com/example/accessmanagementsystem/controller/EventController.java
+++ src/main/java/com/example/accessmanagementsystem/controller/EventController.java
@@ -1,19 +1,18 @@
 package com.example.accessmanagementsystem.controller;
 
 import com.example.accessmanagementsystem.service.contract.EventServiceContract;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 
 @RestController
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class EventController {
 
     private final EventServiceContract eventService;
-
-    public EventController(EventServiceContract eventService) {
-        this.eventService = eventService;
-    }
 
     @GetMapping(path = "/api/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> streamEvents() {
